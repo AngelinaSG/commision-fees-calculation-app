7735b0128358419be91a6eb1cf742108a45d2169
Angelina <smmlinchik@gmail.com>
2023-03-28 14:10:19 +0300

    refactor: update configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,13 @@ module.exports = {
     es2021: true,
   },
   extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  rules: {},
+  rules: {
+    'prettier/prettier': ['error'],
+  },
 };
--- jest.config.ts
+++ jest.config.ts
@@ -1,42 +1,14 @@
-// import { Config } from '@jest/types';
-//
-// const config: Config.InitialOptions = {
-//   preset: "ts-jest",
-//   rootDir: "src",
-//   testEnvironment: "node",
-//   verbose: true,
-//   automock: true,
-//   collectCoverage: true,
-//   collectCoverageFrom: [
-//     "src/**/*.(js,jsx)",
-//     "src/**/*.(ts,tsx)",
-//     "!vendor/**/*.(js,jsx)",
-//     "!**/node_modules/**"
-//   ],
-//   coverageThreshold: {
-//     global: {
-//       branches: 100,
-//       functions: 100,
-//       lines: 100,
-//       statements: 100
-//     }
-//   },
-// }
-//
-// export default config;
+import type { JestConfigWithTsJest } from 'ts-jest';
 
-import type { JestConfigWithTsJest } from 'ts-jest'
-
-import { defaults as tsjPreset } from 'ts-jest/presets'
+import { defaults as tsjPreset } from 'ts-jest/presets';
 
 const jestConfig: JestConfigWithTsJest = {
   verbose: true,
   collectCoverage: true,
-  testEnvironment: "node",
+  testEnvironment: 'node',
   transform: {
     ...tsjPreset.transform,
-    // [...]
   },
-}
+};
 
-export default jestConfig
+export default jestConfig;
